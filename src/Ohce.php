c1683372336f655c1a0741fef1af1a22a4d28069
Jesus Lezaun <lezaun.132418@e.unavarra.es>
2022-03-22 15:12:33 +0000

    Commit11 Refactor ohce

--- src/Ohce.php
+++ src/Ohce.php
@@ -16,33 +16,71 @@ class Ohce
 
     public function execute(string $inputString): string
     {
-        if(strtok($inputString, " ") === "ohce")
-        {
-            $hour = intval($this->timeManager->getTime());
-            $this->name = substr($inputString,
-                strpos($inputString, " ") + 1,
-                strlen($inputString) - strpos($inputString, " ") + 1);
-
-            if(6 < $hour && $hour < 12)
-                return "¡Buenos dias " . $this->name . "!";
-            elseif(12 < $hour && $hour < 20)
-                return "¡Buenas tardes " . $this->name . "!";
-            else
-                return "¡Buenas noches " . $this->name . "!";
+        if ($this->isInitialMessage($inputString)) {
+            return $this->initialMessageHandler($inputString);
         }
 
-        if($inputString === "Stop!")
-        {
-            if($this->name === "")
-                return "Adios";
-            else
-                return "Adios " . $this->name;
+        if ($this->isStopMessage($inputString)) {
+            return $this->stopMessageHandler();
         }
 
         $reversedWord = strrev($inputString);
-        if($reversedWord === $inputString)
+        if ($reversedWord === $inputString) {
             return $reversedWord . ", ¡Bonita palabra!";
+        }
 
         return $reversedWord;
+    }
+
+    /**
+     * @return string
+     */
+    private function stopMessageHandler(): string
+    {
+        if ($this->name === "") {
+            return "Adios";
+        } else {
+            return "Adios " . $this->name;
+        }
+    }
+
+    /**
+     * @param string $inputString
+     * @return bool
+     */
+    private function isStopMessage(string $inputString): bool
+    {
+        return $inputString === "Stop!";
+    }
+
+    /**
+     * @param string $inputString
+     * @return string
+     */
+    private function initialMessageHandler(string $inputString): string
+    {
+        $hour = intval($this->timeManager->getTime());
+        $this->name = substr(
+            $inputString,
+            strpos($inputString, " ") + 1,
+            strlen($inputString) - strpos($inputString, " ") + 1
+        );
+
+        if (6 < $hour && $hour < 12) {
+            return "¡Buenos dias " . $this->name . "!";
+        } elseif (12 < $hour && $hour < 20) {
+            return "¡Buenas tardes " . $this->name . "!";
+        } else {
+            return "¡Buenas noches " . $this->name . "!";
+        }
+    }
+
+    /**
+     * @param string $inputString
+     * @return bool
+     */
+    private function isInitialMessage(string $inputString): bool
+    {
+        return strtok($inputString, " ") === "ohce";
     }
 }
